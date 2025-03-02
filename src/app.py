70b3c54ad3efabbbcc9ef38bdc9080a23263fb54
Heidi Holappa <heidi.holappa@helsinki.fi>
2025-03-02 13:07:26 +0200

    Fix issue with debug logging not showing in debug mode

--- src/app.py
+++ src/app.py
@@ -3,9 +3,12 @@ from flask import Flask
 
 logger = logging.getLogger(__name__)
 FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
-logging.basicConfig(format=FORMAT, level=logging.DEBUG)
-
 
 app = Flask(__name__)
+
+if app.debug:
+    logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+else:
+    logging.basicConfig(format=FORMAT, level=logging.INFO)
 
 from controller import view
